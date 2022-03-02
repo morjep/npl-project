448de7859423a7fa5a091948b6af2c42fe447c63
morjep <94778792+morjep@users.noreply.github.com>
2022-03-02 13:19:14 +0100

    Updated webpack prod config. Currently not working!

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,49 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/client/index.js',
-  mode: 'production',
+  entry: "./src/client/index.js",
+  mode: "production",
   output: {
-    libraryTarget: 'var',
-    library: 'Client',
+    libraryTarget: "var",
+    library: "Client",
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
-        test: '/\.js$/',
+        test: "/.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
     ],
   },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/client/views/index.html',
-      filename: './index.html',
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
     }),
-  ],
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    })
+  ]
 };
