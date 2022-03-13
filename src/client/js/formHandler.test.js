63365e5b31ac062fb2fcf387fab7eded765d234c
morjep <94778792+morjep@users.noreply.github.com>
2022-03-13 08:01:26 +0100

    Added handlesubmit testcase

---
+++ src/client/js/formHandler.test.js
@@ -0,0 +1,8 @@
+import handleSubmit from './formHandler';
+
+describe("Testing the submit functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+           expect(handleSubmit).toBeDefined();
+})});
+
+//Note: The handleSubmit needs more test-cases to increase coverage.
